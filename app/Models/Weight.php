9ab982bdd1a7ecb7596eda7730654eff4fba6f74
dlutz494 <dlutz494@gmail.com>
2024-04-16 16:01:31 -0400

    Add weight model and migration

---
+++ app/Models/Weight.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Weight extends Model
+{
+    use HasFactory;
+
+    protected $table = 'context_encounters';
+
+    protected $attributes = [
+        'context_id'   => '',
+        'encounter_id' => '',
+        'weight'       => 0,
+    ];
+
+    protected $fillable = [
+        'context_id',
+        'encounter_id',
+        'weight',
+    ];
+
+    public function context(): HasOne
+    {
+        return $this->hasOne(Context::class, 'id', 'context_id');
+    }
+
+    public function encounter(): HasOne
+    {
+        return $this->hasOne(Encounter::class, 'id', 'encounter_id');
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'context_id'   => 'integer',
+            'encounter_id' => 'integer',
+            'weight'       => 'integer',
+        ];
+    }
+}
---
+++ database/migrations/2024_04_16_193814_create_context_encounters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Context;
+use App\Models\Encounter;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('context_encounters', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Context::class);
+            $table->foreignIdFor(Encounter::class);
+            $table->integer('weight')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('context_encounters');
+    }
+};
