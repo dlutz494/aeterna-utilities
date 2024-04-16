3a38a3a448297bc5883b5d5a336743fc5e946261
dlutz494 <dlutz494@gmail.com>
2024-04-15 21:38:23 -0400

    Refactor EncounterTable.vue styling

--- resources/js/Pages/EncounterGenerator/EncounterTable.vue
+++ resources/js/Pages/EncounterGenerator/EncounterTable.vue
@@ -7,15 +7,15 @@ const props = defineProps(
 </script>
 
 <template>
-    <table class="mb-4 table-fixed border border-slate-500">
-        <thead class="p-2 bg-gray-950">
+    <table class="mb-4 border border-slate-500">
+        <thead class="bg-gray-400 dark:bg-gray-950">
         <tr>
             <th class="dark:text-white border border-slate-500">Title</th>
             <th class="dark:text-white border border-slate-500">Description</th>
         </tr>
         </thead>
-        <tbody class="mb-2">
-        <tr v-for="encounter in encounters" class="odd:bg-slate-800 even:bg-slate-900">
+        <tbody class="bg-slate-200 dark:bg-slate-700">
+        <tr v-for="encounter in encounters" class="even:bg-slate-300 dark:even:bg-slate-800">
             <td class="dark:text-white pl-4 w-1/6 border border-slate-500 mx-1">{{ encounter.title }}</td>
             <td class="dark:text-white pl-4 w-4/6 border border-slate-500 mx-1">{{ encounter.description }}</td>
         </tr>
