b9b24b7b2edc8b20c8baf69108320c6de006434d
dlutz494 <dlutz494@gmail.com>
2024-04-26 16:15:26 -0400

    Set context_id to null on ContextEncounters when a Context is deleted

--- app/Models/Context.php
+++ app/Models/Context.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Context extends Model
 {
@@ -19,6 +20,21 @@ class Context extends Model
     protected $fillable = [
         'title',
     ];
+
+    protected static function booted(): void
+    {
+        static::deleted(function (Context $context) {
+            foreach ($context->contextEncounters as $contextEncounter) {
+                $contextEncounter->context_id = null;
+                $contextEncounter->save();
+            }
+        });
+    }
+
+    public function contextEncounters(): HasMany
+    {
+        return $this->hasMany(ContextEncounter::class);
+    }
 
     public function encounters(): BelongsToMany
     {
