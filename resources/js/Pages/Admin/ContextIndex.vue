bc8ce9dbac8fdd2ecfdf1e6897896efe1d464133
dlutz494 <dlutz494@gmail.com>
2024-05-22 16:34:51 -0400

    Update edit and delete button styling in index views, justify text left in tables

--- resources/js/Pages/Admin/ContextIndex.vue
+++ resources/js/Pages/Admin/ContextIndex.vue
@@ -30,23 +30,24 @@ const props = defineProps(
             <table class="w-2/3 bg-white text-sm my-4">
                 <thead class="border-b-[3px] border-b-stone-300">
                 <tr>
-                    <th class="border text-center p-4">Title</th>
-                    <th class="border text-center p-4 italic font-light">Actions</th>
+                    <th class="border text-start p-4">Title</th>
+                    <th class="border text-center p-4 italic font-normal">Actions</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr v-for="context in contexts" class="odd:bg-stone-100">
-                    <td class="border text-center px-4 w-3/4 py-2">{{ context.title }}</td>
+                    <td class="border text-start px-4 w-3/4 py-2">{{ context.title }}</td>
                     <td class="border text-center px-4 py-2">
                         <Link
-                            class="rounded-sm p-1 mx-1 text-white bg-stone-600 hover:bg-stone-500 active:bg-stone-700"
+                            class="hover:text-gray-700"
                             :href="context.edit_url"
                             as="button"
                         >
                             Edit
                         </Link>
+                        |
                         <Link
-                            class="rounded-sm p-1 mx-1 text-white bg-red-600 hover:bg-red-500 active:bg-red-700"
+                            class="hover:text-gray-700"
                             :href="context.delete_url"
                             method="delete"
                             as="button"
--- resources/js/Pages/Admin/EncounterIndex.vue
+++ resources/js/Pages/Admin/EncounterIndex.vue
@@ -33,31 +33,32 @@ const props = defineProps(
             <table class="w-2/3 bg-white text-sm my-4">
                 <thead class="border-b-[3px] border-b-stone-300">
                 <tr>
-                    <th class="border text-center p-4">Title</th>
+                    <th class="border text-start p-4">Title</th>
                     <th class="border text-start p-4">Description</th>
-                    <th class="border text-center p-4">Context</th>
-                    <th class="border text-center p-4">Weight</th>
+                    <th class="border text-start p-4">Context</th>
+                    <th class="border text-start p-4">Weight</th>
                     <th class="border text-center p-4 italic font-normal">Actions</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr v-for="encounter in encounters" class="odd:bg-stone-100">
-                    <td class="border text-center px-4 py-2">{{ encounter.title }}</td>
+                    <td class="border text-start px-4 py-2">{{ encounter.title }}</td>
                     <td class="border text-start w-1/2 px-4 py-2">{{ encounter.description }}</td>
-                    <td class="border text-center px-4 py-2">
+                    <td class="border text-start px-4 py-2">
                         {{ encounter.context ? encounter.context.title : 'N/A' }}
                     </td>
-                    <td class="border text-center px-4 py-2">{{ encounter.weight }}</td>
+                    <td class="border text-start px-4 py-2">{{ encounter.weight }}</td>
                     <td class="border text-center px-4 py-2">
                         <Link
-                            class="rounded-sm p-1 mx-1 text-white bg-stone-600 hover:bg-stone-500 active:bg-stone-700"
+                            class="hover:text-gray-700"
                             :href="encounter.edit_url"
                             as="button"
                         >
                             Edit
                         </Link>
+                        |
                         <Link
-                            class="rounded-sm p-1 mx-1 text-white bg-red-600 hover:bg-red-500 active:bg-red-700"
+                            class="hover:text-gray-700"
                             :href="encounter.delete_url"
                             method="delete"
                             as="button"
