216863cd65304e7682a6480ea6144628e9f77c17
dlutz494 <dlutz494@gmail.com>
2024-05-24 16:44:27 -0400

    Fix background image not displaying on edit pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,22 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        './resources/js/**/*.vue'
     ],
 
     theme: {
         extend: {
             backgroundImage: {
-                'site-body': "url('images/background_texture.png')",
+                'site-body': 'url("/images/background_texture.png")'
             },
             fontFamily: {
                 // sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-                sans: ['Roboto', 'Helvetica', 'sans-serif', ...defaultTheme.fontFamily.sans],
-            },
-        },
+                sans: ['Roboto', 'Helvetica', 'sans-serif', ...defaultTheme.fontFamily.sans]
+            }
+        }
     },
 
     plugins: [forms],
 
-    darkMode: ['selector', '[data-theme="dark"]'],
+    darkMode: ['selector', '[data-theme="dark"]']
 };
