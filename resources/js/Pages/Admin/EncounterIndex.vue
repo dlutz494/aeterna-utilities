b4e64762401d019b97bd287b6329e1150e4fbb24
dlutz494 <dlutz494@gmail.com>
2024-06-04 16:32:40 -0400

    Add button to clear sorting on encounter index

--- resources/js/Pages/Admin/EncounterIndex.vue
+++ resources/js/Pages/Admin/EncounterIndex.vue
@@ -89,6 +89,13 @@ const sortTitles = () => {
             break;
     }
 };
+
+const clearSorting = () => {
+    document.getElementById('context-selector').value = ''
+    contextFilter.value = '';
+    weightSorting.value = '';
+    titleSorting.value = '';
+};
 </script>
 
 <template>
@@ -128,13 +135,16 @@ const sortTitles = () => {
                         </option>
                     </select></th>
                     <th class="border"></th>
-                    <th class="border"></th>
+                    <th class="border font-bold text-white hover:cursor-pointer bg-sky-400 hover:bg-sky-300 active:bg-sky-400"
+                        @click="clearSorting">Clear Sorting
+                    </th>
                 </tr>
                 <tr>
                     <th class="border text-start p-4 hover:cursor-pointer w-1/5" @click="sortTitles">Title {{
                             titleSorting.valueOf() === 'ASC' ? '▲' :
                                 titleSorting.valueOf() === 'DESC' ? '▼' : ''
-                        }}</th>
+                        }}
+                    </th>
                     <th class="border text-start p-4 w-1/2">Description</th>
                     <th class="border text-start p-4 w-[10%]">Context</th>
                     <th class="border text-start p-4 hover:cursor-pointer w-[10%]" @click="sortWeights">Weight {{
