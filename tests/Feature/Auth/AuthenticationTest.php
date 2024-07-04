c6ece395e0220d82da4bbefd9e1f93c24840c197
dlutz494 <dlutz494@gmail.com>
2024-07-04 16:17:56 -0400

    Testing it prevents merging when a test is failing

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -14,7 +14,7 @@ class AuthenticationTest extends TestCase
     {
         $response = $this->get('/login');
 
-        $response->assertStatus(200);
+        $response->assertStatus(500);
     }
 
     public function test_users_can_authenticate_using_the_login_screen(): void
