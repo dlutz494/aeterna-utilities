2607de14f71647903d1b5389349078d276dd64f2
dlutz494 <dlutz494@gmail.com>
2024-07-17 16:43:39 -0400

    Add tests for admin panel navigation

--- cypress/e2e/admin-panel/admin_panel.cy.js
+++ cypress/e2e/admin-panel/admin_panel.cy.js
@@ -4,15 +4,21 @@ describe('The Admin Panel', () => {
     });
 
     it('displays the header title', () => {
-        cy.visit('/admin');
-
         cy.getBySel('site-header').should('contain.text', 'Admin');
     });
 
     it('displays the admin menu', () => {
-        cy.visit('/admin');
-
         cy.getBySel('encounters-link').should('contain.text', 'Encounters');
         cy.getBySel('contexts-link').should('contain.text', 'Contexts');
     });
+
+    it('navigates to encounter index', () => {
+        cy.getBySel('encounters-link').click();
+        cy.url().should('contain', 'encounters');
+    });
+
+    it('navigates to context index', () => {
+        cy.getBySel('contexts-link').click();
+        cy.url().should('contain', 'contexts');
+    })
 });
