330ca12d22694540c28dcfff9df73b9d3b021df8
dlutz494 <dlutz494@gmail.com>
2024-07-18 16:39:55 -0400

    Add tests for navbar links

--- cypress/e2e/default_layout.cy.js
+++ cypress/e2e/default_layout.cy.js
@@ -4,17 +4,45 @@ describe('The Default Layout', () => {
     });
 
     it('displays the header', () => {
-        cy.get('[data-cy="site-header"]').should('contain.text', 'Home');
+        cy.getBySel('site-header').should('contain.text', 'Home');
     });
 
     it('displays the footer', () => {
-        cy.get('[data-cy="site-footer"]').should('contain', '2024 David Lutz');
+        cy.getBySel('site-footer').should('contain', '2024 David Lutz');
     });
 
     it('displays the navbar', () => {
-        cy.get('[data-cy="nav-home"]').should('contain.text', 'Home');
-        cy.get('[data-cy="nav-generator"]').should('contain.text', 'Generator');
-        cy.get('[data-cy="nav-admin"]').should('contain.text', 'Admin Panel');
-        cy.get('[data-cy="nav-theme-toggle"]').should('contain.text', 'Toggle Theme:');
+        cy.getBySel('nav-home').should('contain.text', 'Home');
+        cy.getBySel('nav-generator').should('contain.text', 'Generator');
+        cy.getBySel('nav-admin').should('contain.text', 'Admin Panel');
+        cy.getBySel('nav-theme-toggle').should('contain.text', 'Toggle Theme:');
     });
+
+    it('navbar navigates to home', () => {
+        cy.visit('/generator')
+        cy.getBySel('nav-home').click()
+
+        cy.url().should('equal', Cypress.config().baseUrl + '/')
+    });
+
+    it('navbar navigates to generator', () => {
+        cy.getBySel('nav-generator').click()
+
+        cy.url().should('equal', Cypress.config().baseUrl + '/generator')
+    });
+
+    it('navbar navigates to admin panel', () => {
+        cy.getBySel('nav-admin').click()
+
+        cy.url().should('equal', Cypress.config().baseUrl + '/admin')
+    });
+
+    it('toggles the theme', () => {
+        cy.getBySel('nav-theme-toggle')
+            .should('contain.text', 'Toggle Theme: Dark')
+            .click()
+            .should('contain.text', 'Toggle Theme: Light')
+            .click()
+            .should('contain.text', 'Toggle Theme: Dark');
+    })
 });
